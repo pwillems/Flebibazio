c83e27f4f3e4c140743dfa387893e3f49f8a9739
pwillems <willems.pim1@gmail.com>
2017-11-30 23:51:43 +0100

    Menu added

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.SceneManagement;
+
+public class MenuManager : MonoBehaviour {
+
+    public void StartGame()
+    {
+        EditorSceneManager.LoadScene("Infinity Mode");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("We quit the game!");
+    }
+}
